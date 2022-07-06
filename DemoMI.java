d485a6c3231b0594ad418a47aeb4742c86e6835c
Saifullah-119 <104373970+Saifullah-119@users.noreply.github.com>
2022-07-06 12:42:51 +0500

    Add files via upload

---
+++ DemoMI.java
@@ -0,0 +1,23 @@
+interface A
+{
+	
+}
+interface B
+{
+	
+}
+class Parent
+{
+	
+}
+class Child extends Parent implements A,B
+{
+	
+}
+public class DemoMI
+{
+	public static void main(String aggg[])
+	{
+		
+	}
+}
